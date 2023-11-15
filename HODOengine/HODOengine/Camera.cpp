c1c9451c5438d18d93fb7f0d8f5c4a4652c28edd
김민정 <rlaalswjd347@naver.com>
2023-11-15 14:59:05 +0900

    [김민정]가상 클래스는 스마트포인터로 들고 있을 수 없으므로 원시포인터로 교체했다.

--- HODOengine/HODOengine/Camera.cpp
+++ HODOengine/HODOengine/Camera.cpp
@@ -1,13 +1,17 @@
 #include "Camera.h"
+#include "HodoGIObject.h"
+#include "Transform.h"
 using namespace hodoEngine;
-
-
 
 Camera* hodoEngine::Camera::_mainCamera = nullptr;
 
 hodoEngine::Camera::Camera()
 {
-	//_camera = 
+	_camera = HodoGIObject::Instance().factory->CreateCamera();
+	if (_mainCamera == nullptr)
+	{
+		SetMainCamera();
+	}
 }
 
 hodoEngine::Camera* Camera::GetMainCamera()
@@ -18,9 +22,10 @@ hodoEngine::Camera* Camera::GetMainCamera()
 void Camera::SetMainCamera()
 {
 	_mainCamera = this;
+	_camera->SetAsMainCamera();
 }
 
 void Camera::Update()
 {
-
+	_camera->SetWorldTM(GetTransform()->GetWorldTM());
 }
--- HODOengine/HODOengine/Camera.h
+++ HODOengine/HODOengine/Camera.h
@@ -19,7 +19,7 @@ namespace hodoEngine
 	protected:
 		virtual void Update() override;
 	private:
-		std::unique_ptr<hodoGI::ICamera> _camera;
+		hodoGI::ICamera* _camera;
 		static Camera* _mainCamera;
 	};
 }
