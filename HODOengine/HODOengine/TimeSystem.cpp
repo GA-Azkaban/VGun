dc767d2a879e8603afa613a9b99c9f2a35b92778
chloe050412 <chloe980504@gmail.com>
2023-12-04 15:24:53 +0900

    TimeSystem 형변환 수정

--- HODOengine/HODOengine/TimeSystem.cpp
+++ HODOengine/HODOengine/TimeSystem.cpp
@@ -8,16 +8,13 @@ namespace hodoEngine
 	{
 		QueryPerformanceFrequency((LARGE_INTEGER*) & _frequency);
 		QueryPerformanceCounter((LARGE_INTEGER*)&_startTime);
-		
-		// 미리 역수를 취해 매 프레임당 곱셈 연산을 하도록...
-		_timeScale = 1.f / _frequency;
 	}
 
 	void TimeSystem::Update()
 	{
-		QueryPerformanceCounter((LARGE_INTEGER*)&_stopTime);
-		_deltaTime = (((LARGE_INTEGER*)&_stopTime)->QuadPart - ((LARGE_INTEGER*)&_startTime)->QuadPart) * _timeScale;
-		_startTime = _stopTime;
+		QueryPerformanceCounter(&_stopTime);
+		_deltaTime = static_cast<float>(_stopTime.QuadPart - _startTime.QuadPart) / static_cast<double>(_frequency.QuadPart);
+		QueryPerformanceCounter(&_startTime);
 	}
 
 	float TimeSystem::GetDeltaTime()
