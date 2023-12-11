743e8d8b67f337d85b18f2addbd0bddccadceb52
chloe050412 <chloe980504@gmail.com>
2023-12-11 15:50:26 +0900

    Auto stash before merge of "Develop" and "suan--DebugSystem"

---
+++ HODOengine/HODOengine/DebugSystem.cpp
@@ -0,0 +1,41 @@
+﻿#include "DebugSystem.h"
+#include "HodoDebugCube.h"
+#include "GameObject.h"
+
+namespace hodoEngine
+{
+
+
+	void DebugSystem::Initialize()
+	{
+
+	}
+
+	void DebugSystem::Update()
+	{
+		if (_isDebugMode)
+		{
+
+		}
+	}
+
+	void DebugSystem::DrawColliderDebug(hodoData::GameObject* obj, HDMaths::HDFLOAT4& color)
+	{
+		auto component = obj->AddComponent<hodoEngine::DebugCube>();
+		component->Get()->SetFillModeWireframe();
+		//component->Get()->SetColor(color);
+	}
+
+	void DebugSystem::SetDebugOn(int index)
+	{
+		_isDebugMode = true;
+		_index = index;
+		// TODO) �ε����� ���õ� ��ü�� �������ϵ���
+	}
+
+	void DebugSystem::SetDebugOff()
+	{
+		_isDebugMode = false;
+	}
+
+}
