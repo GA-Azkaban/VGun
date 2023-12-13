c41cfa9b4c943342250a525113ad4ccc1a5e2564
kkangddol_KGIT <knemo333@gmail.com>
2023-12-13 16:49:30 +0900

    카메라 컴포넌트 다시 제작

--- HODOengine/HODOengine/Camera.cpp
+++ HODOengine/HODOengine/Camera.cpp
@@ -1,29 +1,178 @@
 #include "Camera.h"
 #include "Transform.h"
-using namespace HDData;
+#include "GameObject.h"
 
-Camera* HDData::Camera::_mainCamera = nullptr;
+#ifdef _DEBUG
+#pragma comment(lib,"..\\x64\\Debug\\HODOmath.lib")
+#else
+#pragma comment(lib,"..\\x64\\Release\\HODOmath.lib")
+#endif
 
-HDData::Camera::Camera()
+namespace HDData
 {
-	if (_mainCamera == nullptr)
+	Camera::Camera()
+		: _nearZ(0.01f),
+		_farZ(1000.0f),
+		_aspect(16.0f / 9.0f),
+		_fovY(70.0f),
+		_nearWindowHeight(),
+		_farWindowHeight()
 	{
-		SetMainCamera();
+
 	}
-}
 
-void Camera::SetMainCamera()
-{
-	_mainCamera = this;
-	//_camera->SetAsMainCamera();
-}
+	void Camera::Start()
+	{
 
-HDData::Camera* Camera::GetMainCamera()
-{
-	return _mainCamera;
-}
+	}
 
-void Camera::Update()
-{
-	//_camera->SetWorldTM(GetTransform()->GetWorldTM());
+	HDEngine::CameraData& Camera::GetCameraData()
+	{
+		// 		float screenWidth = (float)RocketCore::RenderSystem::Instance().GetScreenWidth();
+		// 		float screenHeight = (float)RocketCore::RenderSystem::Instance().GetScreenHeight();
+		// 		_aspect = screenWidth / screenHeight;
+
+
+		_cameraData.nearZ = _nearZ;
+		_cameraData.farZ = _farZ;
+		_cameraData.aspect = _aspect;
+		_cameraData.fovY = _fovY;
+		_cameraData.nearWindowHeight = _nearWindowHeight;
+		_cameraData.farWindowHeight = _farWindowHeight;
+
+		_cameraData.position = GetGameObject()->GetTransform()->GetWorldPosition();
+		_cameraData.rotation = GetGameObject()->GetTransform()->GetWorldRotation();
+
+		return _cameraData;
+	}
+
+	float Camera::GetNearZ() const
+	{
+		return _nearZ;
+	}
+
+	float Camera::GetFarZ() const
+	{
+		return _farZ;
+	}
+
+	float Camera::GetAspect() const
+	{
+		return _aspect;
+	}
+
+	float Camera::GetFovX() const
+	{
+		return HDMath::ToDegree(GetRadianFovX());
+	}
+
+	float Camera::GetRadianFovX() const
+	{
+		float halfWidth = GetNearWindowWidth() / 2;
+		return 2.0f * atan(halfWidth / _nearZ);
+	}
+
+	float Camera::GetFovY() const
+	{
+		return _fovY;
+	}
+
+	float Camera::GetRadianFovY() const
+	{
+		return HDMath::ToRadian(_fovY);
+	}
+
+	float Camera::GetNearWindowWidth() const
+	{
+		return _aspect * _nearWindowHeight;
+	}
+
+	float Camera::GetNearWindowHeight() const
+	{
+		return _nearWindowHeight;
+	}
+
+	float Camera::GetFarWindowWidth() const
+	{
+		return _aspect * _farWindowHeight;
+	}
+
+	float Camera::GetFarWindowHeight() const
+	{
+		return _farWindowHeight;
+	}
+
+	HDMath::HDFLOAT4X4 Camera::GetViewMatrix() const
+	{
+		return _viewMatrix;
+	}
+
+	HDMath::HDFLOAT4X4 Camera::GetProjMatrix() const
+	{
+		return _projMatrix;
+	}
+
+	void Camera::SetNearZ(float nearZ)
+	{
+		_nearZ = nearZ;
+		_cameraData.nearZ = nearZ;
+	}
+
+	void Camera::SetFarZ(float farZ)
+	{
+		_cameraData.farZ = farZ;
+	}
+
+	void Camera::SetAspect(float aspect)
+	{
+		_cameraData.aspect = aspect;
+	}
+
+	void Camera::SetFovY(float fovY)
+	{
+		_cameraData.fovY = fovY;
+	}
+
+	void Camera::Strafe(float delta)
+	{
+		HDMath::HDFLOAT3 rightVec = GetGameObject()->GetTransform()->GetRight();
+		rightVec.x *= delta;
+		rightVec.y *= delta;
+		rightVec.z *= delta;
+
+		GetGameObject()->GetTransform()->Translate(rightVec);
+	}
+
+	void Camera::Walk(float delta)
+	{
+		HDMath::HDFLOAT3 forwardVec = GetGameObject()->GetTransform()->GetForward();
+		forwardVec.x *= delta;
+		forwardVec.y *= delta;
+		forwardVec.z *= delta;
+
+		GetGameObject()->GetTransform()->Translate(forwardVec);
+	}
+
+	void Camera::WorldUpDown(float delta)
+	{
+		HDMath::HDFLOAT3 worldUpDelta = { 0.0f,delta,0.0f };
+		GetGameObject()->GetTransform()->Translate(worldUpDelta);
+	}
+
+	void Camera::Pitch(float radian)
+	{
+		//RMQuaternion newRot = RMRotateQuaternion(gameObject->transform.GetLocalRotation(), gameObject->transform.GetRight(), angle);
+		HDMath::HDFLOAT4 r{ 1.0f,0.0f,0.0f,1.0f };
+		r = HDFloat4MultiplyMatrix(r, GetGameObject()->GetTransform()->GetLocalTM());
+		HDMath::HDQuaternion newRot = HDRotateQuaternion(GetGameObject()->GetTransform()->GetLocalRotation(), { r.x,r.y,r.z }, radian);
+		GetGameObject()->GetTransform()->SetLocalRotation(newRot);
+	}
+
+	void Camera::RotateY(float angle)
+	{
+		HDMath::HDQuaternion newRot = HDRotateQuaternion(GetGameObject()->GetTransform()->GetLocalRotation(), { 0.0f,1.0f,0.0f }, angle);
+		GetGameObject()->GetTransform()->SetLocalRotation(newRot);
+	}
+
+
 }
--- HODOengine/HODOengine/Camera.h
+++ HODOengine/HODOengine/Camera.h
@@ -1,7 +1,7 @@
 #include "dllExporter.h"
 #include "Component.h"
-#include "../HODO3DGraphicsInterface/IHodoGICamera.h"
-#include <memory>
+#include "..\\HODOmath\\HODOmath.h"
+#include "..\\HODO3DGraphicsInterface\\CameraData.h"
 
 /// <summary>
 /// 카메라는 화면에 그려질 그래픽 요소들을 출력하는 데 쓰이는 객체입니다.
@@ -14,12 +14,57 @@ namespace HDData
 	{
 	public:
 		Camera();
-		static Camera* GetMainCamera();
-		void SetMainCamera();
+
 	protected:
-		virtual void Update() override;
+		virtual void Start() override;
+
+	public:
+		HDEngine::CameraData& GetCameraData();
+
 	private:
-		HDEngine::ICamera* _camera;
-		static Camera* _mainCamera;
+		HDEngine::CameraData _cameraData;
+
+	public:
+		float GetNearZ() const;
+		float GetFarZ() const;
+		float GetAspect() const;			// 카메라 비율 screen.width/screen.height
+		float GetFovX() const;				// FovX값을 60분법으로 반환
+		float GetRadianFovX() const;		// FovX값을 호도법으로 반환
+		float GetFovY() const;				// FovY값을 60분법으로 반환
+		float GetRadianFovY() const;		// FovY값을 호도법으로 반환
+
+		float GetNearWindowWidth() const;
+		float GetNearWindowHeight() const;
+		float GetFarWindowWidth() const;
+		float GetFarWindowHeight() const;
+
+		HDMath::HDFLOAT4X4 GetViewMatrix() const;	// 카메라의 로컬좌표'계'를 반환
+		HDMath::HDFLOAT4X4 GetProjMatrix() const;	// 카메라의 투영행렬을 반환
+
+		void SetNearZ(float nearZ);
+		void SetFarZ(float farZ);
+		void SetAspect(float aspect);
+		void SetFovY(float fovY);
+
+	public:
+		void Strafe(float delta);
+		void Walk(float delta);
+		void WorldUpDown(float delta);
+
+		void Yaw(float angle);
+		void Pitch(float radian);
+		void Roll(float angle);
+		void RotateY(float angle);
+
+	private:
+		float _nearZ;				// frustum의 가까운 평면까지의 거리
+		float _farZ;				// frustum의 먼 평면까지의 거리
+		float _aspect;				// 가로 / 세로 비율
+		float _fovY;				// fov각도를 60분법으로 갖고있음
+		float _nearWindowHeight;	// frustum의 가까운 평면의 높이
+		float _farWindowHeight;		// frustum의 먼 평면의 높이
+
+		HDMath::HDFLOAT4X4 _viewMatrix;		// 카메라의 로컬좌표'계'
+		HDMath::HDFLOAT4X4 _projMatrix;		// 카메라의 투영 행렬
 	};
 }
