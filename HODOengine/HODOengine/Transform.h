547d8074c6296d5c920aebb96100476ab4e45580
kkangddol_KGIT <knemo333@gmail.com>
2023-12-13 16:48:44 +0900

    Transform Forward, up, right 갖고오는 함수 추가, Translate 및 Rotate 함수 추가, SetWorldPosition 및 SetWorldRotation float 3개를 매개변수로 받는 오버로딩 함수 추가

--- HODOengine/HODOengine/Transform.h
+++ HODOengine/HODOengine/Transform.h
@@ -23,12 +23,23 @@ namespace HDData
 		HDMath::HDFLOAT3 GetLocalScale() const;
 		HDMath::HDFLOAT4X4 GetWorldTM() const;
 		HDMath::HDFLOAT4X4 GetLocalTM() const;
+		HDMath::HDFLOAT3 GetForward() const;
+		HDMath::HDFLOAT3 GetUp() const;
+		HDMath::HDFLOAT3 GetRight() const;
 		void SetWorldPosition(const HDMath::HDFLOAT3& position);
+		void SetWorldPosition(float x, float y, float z);
 		void SetWorldRotation(const HDMath::HDQuaternion& rotation);
+		void SetWorldRotation(float w, float x, float y, float z);
 		void SetWorldScale(const HDMath::HDFLOAT3& scale);
 		void SetLocalPosition(const HDMath::HDFLOAT3& position);
 		void SetLocalRotation(const HDMath::HDQuaternion& rotation);
 		void SetLocalScale(const HDMath::HDFLOAT3& scale);
+
+	public:
+		void Translate(const HDMath::HDFLOAT3& position);
+		void Translate(float x, float y, float z);
+		void Rotate(float angleX, float angleY, float angleZ);
+		void Rotate(const HDMath::HDQuaternion& quaternion);
 
 	private:
 		HDMath::HDFLOAT3 _position;	// Local position of gameobject
