414bbbaf3047300ec429033ed5ad9af8aaf60c8e
kkangddol_KGIT <knemo333@gmail.com>
2023-12-13 16:52:05 +0900

    변경한 그래픽스 확인하기 위해 TestScene 수정

--- HODOengine/HODOclient/TestScene.cpp
+++ HODOengine/HODOclient/TestScene.cpp
@@ -8,9 +8,9 @@ TestScene::TestScene()
 {
 	_scene = HDEngine::CreateScene("Test");
 
-	auto camera = HDEngine::CreateObject(_scene);
-	camera->AddComponent<MovableCamera>()->SetMainCamera();
-	camera->GetTransform()->SetWorldPosition(HDMath::HDFLOAT3{ 0.0f, 0.0f, -10.0f });
+// 	auto camera = HDEngine::CreateObject(_scene);
+// 	camera->AddComponent<MovableCamera>()->SetMainCamera();
+// 	camera->GetTransform()->SetWorldPosition(HDMath::HDFLOAT3{ 0.0f, 0.0f, -10.0f });
 
 	auto gameObject = HDEngine::CreateObject(_scene);
 // 	auto comp = gameObject->AddComponent<HDEngine::DebugCube>();
