3da336a6e8f03a4c6aac424bc0f992002da18b2e
김민정 <rlaalswjd347@naver.com>
2023-12-15 16:23:04 +0900

    [김민정]MeshRenderer Component 생성

--- HODOengine/HODO3DGraphicsInterface/IHodoGIFactory.h
+++ HODOengine/HODO3DGraphicsInterface/IHodoGIFactory.h
@@ -9,14 +9,16 @@ namespace HDEngine
 {
 	class ICamera;
 	class IDebugCube;
+	class IStaticMesh;
+	class ISkinnedMesh;
 
 	class IFactory
 	{
 	public:
 		virtual ICamera* CreateCamera() = 0;
 		virtual IDebugCube* CreateDebugCube() = 0;
-		//virtual IStaticMesh* CreateStaticMesh() = 0;
-		//virtual ISkinnedMesh* CreateSkinnedMesh() = 0;
+		virtual IStaticMesh* CreateStaticMesh() = 0;
+		virtual ISkinnedMesh* CreateSkinnedMesh() = 0;
 		//virtual IDirectionalLight* CreateDirectionalLight() = 0;
 	};
 }
