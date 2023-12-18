adf6bf0acbb3f93d0c36f7979186f30259efb7c4
chloe050412 <chloe980504@gmail.com>
2023-12-18 11:25:45 +0900

    Plane 콜라이더에 노말 벡터와 Distance 추가

--- HODOengine/HODOengine/StaticPlaneCollider.cpp
+++ HODOengine/HODOengine/StaticPlaneCollider.cpp
@@ -14,3 +14,14 @@ void hodoData::StaticPlaneCollider::Update()
 {
 
 }
+
+HDMaths::HDFLOAT3 hodoData::StaticPlaneCollider::GetNormalVector() const
+{
+	//HDMaths::HDFLOAT3 axis = GetGameObject().GetTransform()->GetRight();
+	return {};
+}
+
+float hodoData::StaticPlaneCollider::GetDistance() const
+{
+	return {};
+}
--- HODOengine/HODOengine/StaticPlaneCollider.h
+++ HODOengine/HODOengine/StaticPlaneCollider.h
@@ -12,6 +12,12 @@ namespace hodoData
 	public:
 		void Start() override;
 		void Update() override;
+
+	public:
+		// plane angle
+		HDMaths::HDFLOAT3 GetNormalVector() const;
+		// how far from bottom
+		float GetDistance() const;
 	};
 }
 
