48f16b97f4021eb9cf96fb681f9b625e18a7f35e
chloe050412 <chloe980504@gmail.com>
2024-01-08 11:08:51 +0900

    [오수안] 버튼 컴포넌트 생성. 클릭 이벤트 함수를 가지고 있음

--- HODOengine/HODOengine/Button.h
+++ HODOengine/HODOengine/Button.h
@@ -1,5 +1,22 @@
 #pragma once
-class Button
+#include <functional>
+
+#include "UIBase.h"
+
+namespace HDData
 {
-};
+	class Button : public UIBase
+	{
+	public:
+		Button();
+
+		void SetOnClickEvent(std::function<void()> event);
+		std::function<void()> GetClickEvent();
+
+	private:
+		std::function<void()> _onClickEvent;
+
+	};
+}
+
 
