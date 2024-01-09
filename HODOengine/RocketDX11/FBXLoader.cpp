fa21d00ef2a06235c9b08dcbb60c3bdef0bb33f3
kkangddol_KGIT <knemo333@gmail.com>
2024-01-09 16:55:03 +0900

    텍스쳐나 그런것들 준비없이 어심프만 했다가 전부 꼬여버렸다. 텍스쳐부터 다시 하자

--- HODOengine/RocketDX11/FBXLoader.cpp
+++ HODOengine/RocketDX11/FBXLoader.cpp
@@ -12,28 +12,28 @@ namespace RocketCore::Graphics
 
 	}
 
-	bool FBXLoader::DoTheImportThing(const std::string& path, Entity* parentEntity)
-	{
-        Assimp::Importer importer;
- 
-        unsigned int flag;
-        flag = aiProcess_Triangulate |
-            aiProcess_JoinIdenticalVertices |
-            aiProcess_CalcTangentSpace |
-            aiProcess_GenNormals |
-            aiProcess_MakeLeftHanded |
-            aiProcess_FlipWindingOrder |
-            aiProcess_FlipUVs;
-
-// 		if (RenderAPI::GetAPI() == RenderAPI::API::DirectX11)
-// 			flag |= aiProcess_FlipUVs;
- 
-        const aiScene* scene = importer.ReadFile(path, flag);
- 
-        if (!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode)
-        {
-            std::string error = "Error:Assimp:";
-            QCAT_ASSERT(false, error + importer.GetErrorString());
-        }
-	}
+// 	bool FBXLoader::DoTheImportThing(const std::string& path, Entity* parentEntity)
+// 	{
+//         Assimp::Importer importer;
+//  
+//         unsigned int flag;
+//         flag = aiProcess_Triangulate |
+//             aiProcess_JoinIdenticalVertices |
+//             aiProcess_CalcTangentSpace |
+//             aiProcess_GenNormals |
+//             aiProcess_MakeLeftHanded |
+//             aiProcess_FlipWindingOrder |
+//             aiProcess_FlipUVs;
+// 
+// // 		if (RenderAPI::GetAPI() == RenderAPI::API::DirectX11)
+// // 			flag |= aiProcess_FlipUVs;
+//  
+//         const aiScene* scene = importer.ReadFile(path, flag);
+//  
+//         if (!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode)
+//         {
+//             std::string error = "Error:Assimp:";
+//             //QCAT_ASSERT(false, error + importer.GetErrorString());
+//         }
+// 	}
 }
