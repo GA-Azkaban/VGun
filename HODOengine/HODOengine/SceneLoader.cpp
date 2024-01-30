846e74ab32e75c0df1aff94e11414e89a17c53e6
kkangddol_KGIT <knemo333@gmail.com>
2024-01-30 18:35:42 +0900

    오브젝트 생성 준비중. 생성해보자!

--- HODOengine/HODOengine/SceneLoader.cpp
+++ HODOengine/HODOengine/SceneLoader.cpp
@@ -6,11 +6,18 @@
 #include <rapidjson/writer.h>
 #include <rapidjson/stringbuffer.h>
 
+#include "SceneLoader.h"
+#include "ObjectSystem.h"
+#include "SceneSystem.h"
+#include "GameObject.h"
+#include "Scene.h"
+#include "MeshRenderer.h"
+
 using rapidjson::Document;
 using rapidjson::SizeType;
 using rapidjson::Value;
 
-#include "SceneLoader.h"
+const std::string RESOURCE_PATH = "Resources/";
 
 namespace HDEngine
 {
@@ -20,6 +27,13 @@ namespace HDEngine
 	}
 
 	void SceneLoader::LoadUnityScene(std::string filePath)
+	{
+		LoadFromJson(filePath);
+		CreateObject();
+		LinkHierachy();
+	}
+
+	void SceneLoader::LoadFromJson(std::string filePath)
 	{
 		// 모든 값을 string으로 묶어서 '한 번' 가져온다
 		std::string serializeDataOnce;
@@ -36,18 +50,52 @@ namespace HDEngine
 		Document doc;
 		doc.Parse(serializeDataOnce.c_str());
 
-// 		std::cout << doc[0]["ID"].GetInt() << std::endl;
-// 		std::cout << doc[1]["ID"].GetInt() << std::endl;
-
 		for (SizeType i = 0; i < doc.Size(); ++i)
 		{
 			const Value& obj = doc[i];
+			InfoStruct info;
 
-			//std::cout << obj["Name"].GetString() << std::endl;
-
-			// 객체 하나를 선언
-			//DataStruct data;
+			info.id = obj["ID"].GetInt();
+			info.parentID = obj["ParentID"].GetInt();
+			info.name = obj["Name"].GetString();
+			info.meshName = obj["MeshName"].GetString();
+			info.position.x = obj["Position"][0].GetFloat();
+			info.position.y = obj["Position"][1].GetFloat();
+			info.position.z = obj["Position"][2].GetFloat();
+			info.rotation.x = obj["Rotation"][0].GetFloat();
+			info.rotation.y = obj["Rotation"][1].GetFloat();
+			info.rotation.z = obj["Rotation"][2].GetFloat();
+			info.rotation.w = obj["Rotation"][3].GetFloat();
+			info.scale.x = obj["Scale"][0].GetFloat();
+			info.scale.y = obj["Scale"][1].GetFloat();
+			info.scale.z = obj["Scale"][2].GetFloat();
+			info.colliderType = obj["ColliderType"].GetInt();
+			info.colliderCenter.x = obj["ColliderCenter"][0].GetFloat();
+			info.colliderCenter.y = obj["ColliderCenter"][1].GetFloat();
+			info.colliderCenter.z = obj["ColliderCenter"][2].GetFloat();
+			info.boxColliderSize.x = obj["BoxColliderSize"][0].GetFloat();
+			info.boxColliderSize.y = obj["BoxColliderSize"][1].GetFloat();
+			info.boxColliderSize.z = obj["BoxColliderSize"][2].GetFloat();
+			info.sphereColliderRadius = obj["SphereColliderRadius"].GetFloat();
 		}
+	}
+
+	void SceneLoader::CreateObject()
+	{
+		HDData::Scene* currentScene = SceneSystem::Instance().GetCurrentScene();
+		for (auto& info : _infoList)
+		{
+			HDData::GameObject* object = currentScene->CreateObject(info.name);
+			HDData::MeshRenderer* meshRenderer = object->AddComponent<HDData::MeshRenderer>();
+			meshRenderer->LoadMesh(RESOURCE_PATH + info.meshName);
+
+			// TODO : 메쉬 넣어보는걸로? 콜라이더도? 나중에 링크도 해줘야해.
+		}
+	}
+
+	void SceneLoader::LinkHierachy()
+	{
+
 	}
 
 }
--- HODOengine/HODOengine/SceneLoader.h
+++ HODOengine/HODOengine/SceneLoader.h
@@ -1,12 +1,47 @@
 ﻿#pragma once
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+#include "MathHeader.h"
+
+namespace HDData
+{
+	class GameObject;
+}
 
 namespace HDEngine
 {
 	class SceneLoader
 	{
+		struct InfoStruct
+		{
+			int id;
+			int parentID;
+			std::string name;
+			std::string meshName;
+			Vector3 position;
+			Quaternion rotation;
+			Vector3 scale;
+			int colliderType;		// box = 1, sphere = 2
+			Vector3 colliderCenter;
+			Vector3 boxColliderSize;
+			float sphereColliderRadius;
+		};
+
 	public:
 		SceneLoader();
 
 		void LoadUnityScene(std::string filePath);
+
+	private:
+		void LoadFromJson(std::string filePath);
+		void CreateObject();
+		void LinkHierachy();
+
+	private:
+		std::vector<InfoStruct> _infoList;
+		std::vector<HDData::GameObject*> _gameObjectList;
+		std::unordered_map<int, HDData::GameObject*> _gameObjectMap;
 	};
 }
