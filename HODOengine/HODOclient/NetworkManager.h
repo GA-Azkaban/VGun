6351444843f48f1762e1d8d52b213b149429e28e
성원 <clover24802@gmail.com>
2024-02-01 01:37:24 +0900

    클라이언트에 네트워크 라이브러리 추가, NetworkManager 로그인 회원가입 기능 추가
    빌드는 되는데 리소스 불러오는 곳에서 에러가 난다 작업 영역이 달라 원래 발생하던 증상인듯

---
+++ HODOengine/HODOclient/NetworkManager.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+#include "../HODOengine/HODO_API.h"
+#include "Types.h"
+#include "Enum.pb.h"
+#include "Struct.pb.h"
+
+class NetworkManager : public HDData::Script
+{
+public:
+	static NetworkManager* Instance();
+
+private:
+	static NetworkManager* _instance;
+	NetworkManager();
+	~NetworkManager() = default;
+
+public:
+	virtual void Start() override;
+	virtual void Update() override;
+
+public:
+	void SendLogin(std::string id, std::string password);
+	void SendCreateAccount(std::string id, std::string password, std::string nickname);
+	
+
+private:
+	Horang::ClientServiceRef _service;
+};
+
---
+++ HODOengine/HODOclient/ServerSession.cpp
@@ -0,0 +1,31 @@
+﻿#include "pch.h"
+#include "ServerSession.h"
+#include "ServerPacketHandler.h"
+#include "NetworkManager.h"
+
+ServerSession::~ServerSession()
+{
+}
+
+void ServerSession::OnConnected()
+{
+	
+}
+
+void ServerSession::OnRecvPacket(BYTE* buffer, int32 len)
+{
+	Horang::PacketSessionRef session = GetPacketSessionRef();
+	Horang::PacketHeader* header = reinterpret_cast<Horang::PacketHeader*>(buffer);
+			
+	ServerPacketHandler::HandlePacket(session, buffer, len);
+}
+
+void ServerSession::OnSend(int32 len)
+{
+
+}
+
+void ServerSession::OnDisconnected()
+{
+
+}
