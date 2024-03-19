6c39129ac30d59db071342811f007453b04ede08
Dispact <tlsdbswp97@naver.com>
2024-03-19 14:16:31 +0900

    [신윤제]네트워크 링크 에러 해결
    processNode _scene이 nullptr

--- HODOengine/HODOclient/pch.h
+++ HODOengine/HODOclient/pch.h
@@ -1,15 +1,15 @@
-#pragma once
+﻿#pragma once
 
 #define WIN32_LEAN_AND_MEAN
 
 #ifdef _DEBUG
 
-#pragma comment(lib,"ServerCore\\Debug\\ServerCore.lib")
-#pragma comment(lib,"ProtoBuf\\Debug\\libprotobufd.lib")
+#pragma comment(lib,"ServerCore.lib")
+#pragma comment(lib,"libprotobufd.lib")
 
 #else
-#pragma comment(lib,"ServerCore\\Release\\ServerCore.lib")
-#pragma comment(lib,"ProtoBuf\\Release\\libprotobuf.lib")
+#pragma comment(lib,"Release\\ServerCore.lib")
+#pragma comment(lib,"Release\\libprotobuf.lib")
 
 #endif
 
