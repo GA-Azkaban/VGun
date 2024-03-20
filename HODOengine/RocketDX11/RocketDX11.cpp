d3285593850bdb229b96a683fa390795e972bf43
chloe050412 <chloe980504@gmail.com>
2024-03-20 11:37:34 +0900

    [오수안] 브랜치 생성을 위한 커밋

--- HODOengine/RocketDX11/RocketDX11.cpp
+++ HODOengine/RocketDX11/RocketDX11.cpp
@@ -145,8 +145,8 @@ namespace RocketCore::Graphics
 		_resourceManager.Initialize(_device.Get(), _deviceContext.Get());
 
 		/// Load resources
-		_resourceManager.LoadFBXFile("A_TP_CH_Breathing.fbx");
-		_resourceManager.LoadFBXFile("A_TP_CH_Sprint_F.fbx");
+		//_resourceManager.LoadFBXFile("A_TP_CH_Breathing.fbx");
+		//_resourceManager.LoadFBXFile("A_TP_CH_Sprint_F.fbx");
 		_resourceManager.LoadCubeMapTextureFile("sunsetcube1024.dds");
 		_resourceManager.LoadCubeMapTextureFile("Day Sun Peak Clear.dds");
 
