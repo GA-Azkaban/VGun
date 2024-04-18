78f1e6c9cd4c8ad1774d4120b8e41667792903f2
chloe050412 <chloe980504@gmail.com>
2024-04-18 20:48:17 +0900

    [오수안] 디벨롭 시작 씬 수정

--- HODOengine/HODOclient/Client.cpp
+++ HODOengine/HODOclient/Client.cpp
@@ -1,4 +1,4 @@
-#include "Client.h"
+﻿#include "Client.h"
 #include "TestScene.h"
 #include "LoginSceneView.h"
 #include "MainMenu.h"
@@ -20,8 +20,8 @@ void Client::Initialize()
 {
 	//NetworkManager::Instance();
 
-	TestScene test;
-	test.Start();
+	//TestScene test;
+	//test.Start();
 
 	//LoginSceneView login;
 	//login.Initialize();
@@ -29,8 +29,8 @@ void Client::Initialize()
 	//MainMenuScene menu;
 	//menu.Initalize();
 
-	//UnitySceneLoaderTest unity;
-	//unity.Start();
+	UnitySceneLoaderTest unity;
+	unity.Start();
 
 	//A a;
 	//a.Initialize();
