c7bb9be8b9ca23c577b065adb958ee1080d44fb6
Dispact <tlsdbswp97@naver.com>
2024-04-19 19:26:15 +0900

    [신윤제] 귀가 커밋

--- HODOengine/RocketDX11/StaticMeshObject.cpp
+++ HODOengine/RocketDX11/StaticMeshObject.cpp
@@ -41,6 +41,10 @@ namespace RocketCore::Graphics
 	void StaticMeshObject::LoadMesh(const std::string& fileName)
 	{
 		m_meshes = ResourceManager::Instance().GetMeshes(fileName);
+
+
+
+
 		m_materials = ResourceManager::Instance().GetMaterials(fileName);
 		m_node = ResourceManager::Instance().GetNode(fileName);
 	}
