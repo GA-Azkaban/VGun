19a6c012cd46f1b0b0531b486b92dbdec0b943fa
chloe050412 <chloe980504@gmail.com>
2024-05-22 13:40:13 +0900

    []

--- HODOengine/HODOclient/NetworkManager.cpp
+++ HODOengine/HODOclient/NetworkManager.cpp
@@ -397,16 +397,6 @@ void NetworkManager::RecvGameStart()
 	API::LoadSceneByName("InGame");
 }
 
-void NetworkManager::SendPlayerTranform()
-{
-
-}
-
-void NetworkManager::RecvPlayerTranform()
-{
-
-}
-
 void NetworkManager::SendPlayUpdate(Protocol::PlayerData playerData)
 {
 	Protocol::C_PLAY_UPDATE packet;
