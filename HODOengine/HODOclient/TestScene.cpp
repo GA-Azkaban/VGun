d6143f42097e00ada5e5ba3e43e6bb29315cff84
chloe050412 <chloe980504@gmail.com>
2024-06-24 11:41:28 +0900

    [오수안] 애니메이션 테스트용 커밋

--- HODOengine/HODOclient/TestScene.cpp
+++ HODOengine/HODOclient/TestScene.cpp
@@ -212,20 +212,23 @@ TestScene::TestScene()
 	// 플레이어 테스트
 	auto playerTest = API::CreateObject(_scene, "player");
 	playerTest->GetComponent<HDData::Transform>()->SetPosition(Vector3{ 0.0f, 0.0f, 0.0f });
-	//playerTest->AddComponent<PlayerTest>();
+	playerTest->AddComponent<PlayerTest>();
 	// 확장자 포함한 파일이름을 넣어준다.
 	// LoadFBXFile 함수는 노드를 따라 게임오브젝트를 계층구조대로 생성해주고
 	// 메쉬와 노드를 불러와 적용시킨다.
 	// 그리고 자식오브젝트를 만들어 SkinnedMeshRenderer 컴포넌트를 부착한다.
 	//playerTest->LoadFBXFile("SKM_TP_X_Default.fbx");
 	playerTest->LoadFBXFile("SKM_CowboyTP_X_default.fbx");
+	
+	auto mesh = playerTest->GetComponentInChildren<HDData::SkinnedMeshRenderer>();
+	mesh->LoadAnimation("TP");
+	mesh->PlayAnimation("RV_idle.fbx", true, 0.1, true, 0.1);
 
 	//playerTest->AddComponent<HDData::Animator>();
 	//API::LoadFPAnimationFromData(playerTest, "TP_animation.json");
 
 	//playerTest->AddComponent<TPScript>();
 
-	playerTest->GetComponentInChildren<HDData::SkinnedMeshRenderer>()->PlayAnimation("SKM_TP_RV_idle.fbx", true, 0.1, true, 0.1);
 
 	//// SkinnedMeshRenderer 컴포넌트는 자식오브젝트에 생성되므로
 	//// GetComponentInChildren 함수로 가져와서 사용해야 한다.
