54d006ec35e622721a30451aedfc8a7939ac714e
김민정 <rlaalswjd347@naver.com>
2024-07-03 12:13:55 +0900

    저장

---
+++ HODOengine/HODOclient/HitEffect.cpp
@@ -0,0 +1,2 @@
+﻿#include "HitEffect.h"
+
--- HODOengine/HODOclient/HitEffect.h
+++ HODOengine/HODOclient/HitEffect.h
@@ -1,1 +1,20 @@
-#pragma once
+﻿#pragma once
+#include "../HODOengine/HODO_API.h"
+#include "Timer.h"
+
+class GameManager;
+
+class HitEffect : public HDData::Script
+{
+public:
+	HitEffect();
+
+	virtual void Start() override;
+	virtual void Update() override;
+
+private:
+	GameManager* _gameManager;
+	HDData::ImageUI* _hitEffectImage;
+	Timer _hitEffectTimer;
+	HDData::AnimationCurve _curve;
+};
