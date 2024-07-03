96a5263f128d55b4ec2fc3a2ad3644fcc83c6a83
DESKTOP-7F78D4D\신윤제 <tlsdbswp97@naver.com>
2024-07-03 23:50:17 +0900

    배열 추가

--- HODOengine/HODOengine/SceneLoader.h
+++ HODOengine/HODOengine/SceneLoader.h
@@ -50,6 +50,9 @@ namespace HDEngine
 		// 스폰 지점
 		Vector3 _spawnPoint[20];
 		int _spawnIndex = 1;
+
+		// 구름 위치 지정
+		Vector3 _cloudPosition[30];
 	private:
 		HDData::Scene * now;
 		HDData::Material* _material;
