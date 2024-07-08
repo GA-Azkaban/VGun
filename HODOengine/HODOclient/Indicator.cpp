a11b6fa2041b4ae016cd9f486509a38cbfa1d4a1
김민정 <rlaalswjd347@naver.com>
2024-07-08 11:33:52 +0900

    [김민정]인디케이터 기능 추가

--- HODOengine/HODOclient/Indicator.cpp
+++ HODOengine/HODOclient/Indicator.cpp
@@ -7,35 +7,51 @@ Indicator::Indicator()
 {
 	_indicatorTimer.isRepeat = false;
 	_indicatorTimer.duration = 1.0f;
+	_indicatorTimer.onUpdate = [&](float progress) {
+		auto playerObj = IndicatorPool::Instance().player;
+		Vector3 forward = playerObj->GetTransform()->GetForward();
+		Vector3 targetDir = _targetPos - playerObj->GetTransform()->GetPosition();
+		// Calculate degree between two vectors
+		targetDir.Normalize();
+		float scalar = forward.x * targetDir.x + forward.z * targetDir.z;
+		float length1 = forward.Length();
+		float length2 = targetDir.Length();
+		float radian = acosf(scalar / (length1 * length2));
+		// if cross product y value is negative, reverse the angle
+		Vector3 crossVec = forward.Cross(targetDir);
+		if (crossVec.y < 0)
+		{
+			float degree = radian * 180.0f / std::numbers::pi;
+			radian = (360.0f - degree) * std::numbers::pi / 180.0f;
+		}
+		_indicatorImage->SetAngle(radian); };
 	_indicatorTimer.onExpiration = [&]() { IndicatorPool::Instance().RetrieveIndicator(this); };
 }
 
-float Indicator::CalculateDegreeBetweenTwoVectors(Vector3 v1, Vector3 v2)
-{
-	v1.Normalize();
-	v2.Normalize();
-
-	float scalar = v1.x * v2.x + v1.z * v2.z;
-	float length1 = v1.Length();
-	float length2 = v2.Length();
-	float radian = acosf(scalar / (length1 * length2));
-
-	Vector3 crossVec = v1.Cross(v2);
-	//float mul = v1.x * v2.y - v1.y * v2.x;
-
-	//if (mul * crossVec.z >= 0)
-	if(crossVec.y >= 0)
-	{
-		float degree = radian * 180.0f / std::numbers::pi;
-		return radian;
-	}
-	else
-	{
-		float degree = radian * 180.0f / std::numbers::pi;
-		float resultRadian = (360.0f - degree) * std::numbers::pi / 180.0f;
-		return resultRadian;
-	}
-}
+//float Indicator::CalculateDegreeBetweenTwoVectors(Vector3 v1, Vector3 v2)
+//{
+//	v1.Normalize();
+//	v2.Normalize();
+//
+//	float scalar = v1.x * v2.x + v1.z * v2.z;
+//	float length1 = v1.Length();
+//	float length2 = v2.Length();
+//	float radian = acosf(scalar / (length1 * length2));
+//
+//	Vector3 crossVec = v1.Cross(v2);
+//	
+//	if(crossVec.y >= 0)
+//	{
+//		//float degree = radian * 180.0f / std::numbers::pi;
+//		return radian;
+//	}
+//	else
+//	{
+//		float degree = radian * 180.0f / std::numbers::pi;
+//		float resultRadian = (360.0f - degree) * std::numbers::pi / 180.0f;
+//		return resultRadian;
+//	}
+//}
 
 Indicator* Indicator::CreateIndicator()
 {
@@ -57,24 +73,11 @@ Indicator* Indicator::CreateIndicator()
 
 void Indicator::Update()
 {
-	// 각도 계산
-	auto playerObj = IndicatorPool::Instance().player;
-	Vector3 targetDir = _targetPos - playerObj->GetTransform()->GetPosition();
-	Vector3 forward = IndicatorPool::Instance().player->GetTransform()->GetForward();
-	float radian = CalculateDegreeBetweenTwoVectors(forward, targetDir);
-	_indicatorImage->SetAngle(radian);
 	_indicatorTimer.Update();
 }
 
 void Indicator::SetEffectOn(Vector3 targetPos)
 {
-	// 각도 계산
 	_targetPos = targetPos;
-	auto playerObj = IndicatorPool::Instance().player;
-	Vector3 playerPos = playerObj->GetTransform()->GetPosition();
-	Vector3 targetDir = _targetPos - playerPos;
-	Vector3 forward = playerObj->GetTransform()->GetForward();
-	float radian = CalculateDegreeBetweenTwoVectors(forward, targetDir);
-	_indicatorImage->SetAngle(radian);
 	_indicatorTimer.Start();
 }
