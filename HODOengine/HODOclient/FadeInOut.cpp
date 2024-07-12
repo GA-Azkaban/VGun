b579debb30bcfe19a0054fc443a6ee3e09f52234
Dispact <tlsdbswp97@naver.com>
2024-07-12 17:35:33 +0900

    [신윤제]페이드인 & 아웃 수정중

--- HODOengine/HODOclient/FadeInOut.cpp
+++ HODOengine/HODOclient/FadeInOut.cpp
@@ -1,36 +1,13 @@
 ﻿#include "FadeInOut.h"
 
-FadeInOut& FadeInOut::Instance()
-{
-	if (_instance == nullptr)
-	{
-		_instance = new FadeInOut;
-	}
-
-	return *_instance;
-}
-
-FadeInOut* FadeInOut::_instance = nullptr;
-
 FadeInOut::FadeInOut()
 {
-	UINT width = API::GetScreenWidth();
-	UINT height = API::GetScreenHeight();
 
-	API::CreateStaticComponent(this);
-	_fadeCanvas = API::CreateStaticObject();
-	_fadeCanvas->AddComponent<HDData::ImageUI>();
-	_fadeCanvas->GetComponent<HDData::ImageUI>()->SetImage("black.png");
-	_fadeCanvas->GetComponent<HDData::ImageUI>()->ChangeScale(static_cast<float>(width) / 1920, static_cast<float>(height) / 1080);
-	//_fadeCanvas->GetComponent<HDData::ImageUI>()->ChangeScale(API::GetScreenWidth(), API::GetScreenHeight());
-	_fadeCanvas->GetComponent<HDData::ImageUI>()->SetSortOrder(0.9f);
-	_fadeCanvas->GetTransform()->SetPosition(API::GetScreenWidth() / 2.0f, API::GetScreenHeight() / 2.0f, 0);
-	_fadeCanvas->GetComponent<HDData::ImageUI>()->SetIsIgnoreFocus(true);
 }
 
 void FadeInOut::Start()
 {
-
+	_fadeImage = this->GetGameObject();
 }
 
 void FadeInOut::Update()
@@ -40,16 +17,15 @@ void FadeInOut::Update()
 
 void FadeInOut::FadeIn()
 {
-	_fadeCanvas->GetComponent<HDData::ImageUI>()->FadeIn();
+
 }
 
 void FadeInOut::FadeOut()
 {
-	_fadeCanvas->GetComponent<HDData::ImageUI>()->FadeOut();
 }
 
 bool FadeInOut::GetComplete()
 {
-	return _fadeCanvas->GetComponent<HDData::ImageUI>()->GetComplete();
+
 }
 
--- HODOengine/HODOclient/FadeInOut.h
+++ HODOengine/HODOclient/FadeInOut.h
@@ -4,14 +4,10 @@
 
 class FadeInOut : public HDData::Script
 {
-public:
-	static FadeInOut& Instance();
-
 private:
-	static FadeInOut* _instance;
 	FadeInOut(); 
 	~FadeInOut() = default;
-	HDData::GameObject* _fadeCanvas;
+	HDData::GameObject* _fadeImage = nullptr;
 
 	Timer _fadeInTimer;
 	Timer _fadeOutTimer;
