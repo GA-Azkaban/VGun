222316d407255f807b59334911b8aa403b930c96
chloe050412 <chloe980504@gmail.com>
2024-07-19 19:22:11 +0900

    [오수안] 게임 끝나면 방 바로 터지게 함

--- HODOengine/HODOclient/RoundManager.cpp
+++ HODOengine/HODOclient/RoundManager.cpp
@@ -324,9 +324,10 @@ void RoundManager::ExitGame()
 	_endObj->SetSelfActive(false);
 	_showResultTimer->Stop();
 	_resultTimerUI->GetGameObject()->SetSelfActive(false);
+	NetworkManager::Instance().SendRoomLeave();
 
 	// 로비로 복귀
-	API::LoadSceneByName("Lobby");
+	API::LoadSceneByName("MainMenu");
 	LobbyManager::Instance().RefreshRoom();
 	API::GetCubeMap()->LoadCubeMapTexture("Day Sun Peak Clear Gray.dds");
 	API::GetCubeMap()->SetEnvLightIntensity(1.0f);
